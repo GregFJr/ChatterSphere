c89ac69a75d641f8e31653398be151910ece1e2e
GregFJr <finney101690@gmail.com>
2023-11-11 12:27:15 -0700

    updated the middleware in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3002;
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', routes);
+app.use('/api', routes);
 
 // Test route
 app.get('/test', (req, res) => {
